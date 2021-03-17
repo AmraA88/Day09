e6935c8f1e5d0d0aeea32e26f5b814e37dcd7c5c
AmraA88 <amra_arnaut@hotmail.com>
2021-03-17 09:55:42 +0100

    add Day09

---
+++ ex13/src/index.js
@@ -0,0 +1,10 @@
+function inverseWhile() {
+    fiveNumbers = 5;
+    while (fiveNumbers < 0) {
+        console.log(fiveNumbers);
+        fiveNumbers--;
+    }
+}
+inverseWhile();
+console.log(inverseWhile());
+module.exports = inverseWhile
